2c99c9e11404487cb6804034e0cae78ddcc03d7b
Elena <varlamova.elena200@gmail.com>
2021-10-20 22:50:19 +0300

    Design,interface, buttons, borders of mountains

--- 2DFancyTools/FormMountains.Designer.cs
+++ 2DFancyTools/FormMountains.Designer.cs
@@ -29,12 +29,97 @@ namespace _2DFancyTools
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.canvas = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // canvas
+            // 
+            this.canvas.Location = new System.Drawing.Point(0, 0);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(910, 500);
+            this.canvas.TabIndex = 0;
+            this.canvas.TabStop = false;
+            this.canvas.MouseClick += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseClick);
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.Location = new System.Drawing.Point(23, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 30);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Нарисовать границы";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(23, 99);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 27);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(158, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Коэф. шероховатости";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(23, 145);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(137, 34);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Горы";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(22, 200);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(138, 30);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Очистить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // FormMountains
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(908, 500);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.canvas);
+            this.Name = "FormMountains";
             this.Text = "FormMountains";
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox canvas;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
--- 2DFancyTools/FormMountains.cs
+++ 2DFancyTools/FormMountains.cs
@@ -10,11 +10,87 @@ using System.Windows.Forms;
 
 namespace _2DFancyTools
 {
+   
     public partial class FormMountains : Form
     {
+        bool drawingMode;
         public FormMountains()
+        //Реализовать алгоритм midpoint displacement для двумерной визуализации горного массива.
         {
             InitializeComponent();
+            g = canvas.CreateGraphics();
+           canvas.Image = new Bitmap(1300, 900);
+            drawingMode = false;
+        }
+        List<Point> polygonPoints = new List<Point>();
+
+
+        /// <summary>
+        /// Для рисования
+        /// </summary>
+        SolidBrush blackBrush = new SolidBrush(Color.Black);
+        Pen blackPen = new Pen(Color.Black, 3);
+        Graphics g;
+        
+        void drawPoint(MouseEventArgs e)
+        {
+            
+            
+            polygonPoints.Add(e.Location);
+            
+            g.FillEllipse(blackBrush, e.X - 3, e.Y - 3, 7, 7);
+            
+           //setFlags(isAffineTransformationsEnabled: true);
+        }
+         void canvas_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (drawingMode)
+            {
+                
+               drawPoint(e); 
+                
+            }
+           
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            drawingMode = true;
+        }
+        void DrawMountain()
+        {
+            double r = double.Parse(textBox1.Text);//коэфф шероховатости зададим сами
+            Point p1 = polygonPoints[0];
+            Point p2 = polygonPoints[1];
+            Random rnd = new Random();
+            g.DrawLine(blackPen, p1, p2);
+            int c = (p1.X + p2.X) / 2;
+            double y = (p1.Y + p2.Y) / 2 + rnd.Next((int)(-r * 10), (int)(r * 10));
+            Point p3=new Point(c,(int) y);
+            g.DrawLine(blackPen,p1,p3);
+            g.DrawLine(blackPen, p3, p2);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DrawMountain();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (!drawingMode)
+            {
+                button3.Visible = false;
+            }
+            else
+            {
+                g.Clear(Color.White);
+                polygonPoints.Clear();
+                textBox1.Clear();
+                drawingMode = false;
+            }
+
         }
     }
 }
