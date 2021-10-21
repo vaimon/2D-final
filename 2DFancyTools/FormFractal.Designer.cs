962681d8254b1b9b52d2e2b6285d83ddd11c9de1
Elizaveta Kadobkina <kadobkina@sfedu.ru>
2021-10-21 15:42:07 +0300

    design

--- 2DFancyTools/FormFractal.Designer.cs
+++ 2DFancyTools/FormFractal.Designer.cs
@@ -29,12 +29,162 @@ namespace _2DFancyTools
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.ComboBoxLSystemChange = new System.Windows.Forms.ComboBox();
+            this.textBoxChangeGeneration = new System.Windows.Forms.TextBox();
+            this.labelGeneration = new System.Windows.Forms.Label();
+            this.buttonDrawFractal = new System.Windows.Forms.Button();
+            this.labelRandom = new System.Windows.Forms.Label();
+            this.labelRandomFrom = new System.Windows.Forms.Label();
+            this.labelRandomTo = new System.Windows.Forms.Label();
+            this.textBoxRandomFrom = new System.Windows.Forms.TextBox();
+            this.textBoxRandomTo = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonClear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ComboBoxLSystemChange
+            // 
+            this.ComboBoxLSystemChange.FormattingEnabled = true;
+            this.ComboBoxLSystemChange.Items.AddRange(new object[] {
+            "Кривая Коха",
+            "Снежинка Коха",
+            "Квадратный остров Коха",
+            "Ковер Серпинского",
+            "Наконечник Серпинского",
+            "Кривая Гильберта",
+            "Кривая Дракона",
+            "Шестиугольная кривая Госпера"});
+            this.ComboBoxLSystemChange.Location = new System.Drawing.Point(12, 50);
+            this.ComboBoxLSystemChange.Name = "ComboBoxLSystemChange";
+            this.ComboBoxLSystemChange.Size = new System.Drawing.Size(184, 23);
+            this.ComboBoxLSystemChange.TabIndex = 0;
+            this.ComboBoxLSystemChange.Text = "Выбрать L-систему";
+            this.ComboBoxLSystemChange.SelectedIndexChanged += new System.EventHandler(this.ComboBoxLSystemChange_SelectedIndexChanged);
+            // 
+            // textBoxChangeGeneration
+            // 
+            this.textBoxChangeGeneration.Location = new System.Drawing.Point(11, 131);
+            this.textBoxChangeGeneration.Name = "textBoxChangeGeneration";
+            this.textBoxChangeGeneration.Size = new System.Drawing.Size(100, 23);
+            this.textBoxChangeGeneration.TabIndex = 1;
+            this.textBoxChangeGeneration.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxChangeGeneration_KeyPress);
+            // 
+            // labelGeneration
+            // 
+            this.labelGeneration.AutoSize = true;
+            this.labelGeneration.Location = new System.Drawing.Point(12, 107);
+            this.labelGeneration.Name = "labelGeneration";
+            this.labelGeneration.Size = new System.Drawing.Size(69, 15);
+            this.labelGeneration.TabIndex = 2;
+            this.labelGeneration.Text = "Поколение";
+            // 
+            // buttonDrawFractal
+            // 
+            this.buttonDrawFractal.Location = new System.Drawing.Point(11, 298);
+            this.buttonDrawFractal.Name = "buttonDrawFractal";
+            this.buttonDrawFractal.Size = new System.Drawing.Size(133, 23);
+            this.buttonDrawFractal.TabIndex = 3;
+            this.buttonDrawFractal.Text = "Сделать красивое  с:";
+            this.buttonDrawFractal.UseVisualStyleBackColor = true;
+            this.buttonDrawFractal.Click += new System.EventHandler(this.buttonDrawFractal_Click);
+            // 
+            // labelRandom
+            // 
+            this.labelRandom.AutoSize = true;
+            this.labelRandom.Location = new System.Drawing.Point(11, 198);
+            this.labelRandom.Name = "labelRandom";
+            this.labelRandom.Size = new System.Drawing.Size(134, 15);
+            this.labelRandom.TabIndex = 4;
+            this.labelRandom.Text = "Диапазон случайности";
+            // 
+            // labelRandomFrom
+            // 
+            this.labelRandomFrom.AutoSize = true;
+            this.labelRandomFrom.Location = new System.Drawing.Point(12, 230);
+            this.labelRandomFrom.Name = "labelRandomFrom";
+            this.labelRandomFrom.Size = new System.Drawing.Size(19, 15);
+            this.labelRandomFrom.TabIndex = 5;
+            this.labelRandomFrom.Text = "от";
+            // 
+            // labelRandomTo
+            // 
+            this.labelRandomTo.AutoSize = true;
+            this.labelRandomTo.Location = new System.Drawing.Point(92, 230);
+            this.labelRandomTo.Name = "labelRandomTo";
+            this.labelRandomTo.Size = new System.Drawing.Size(20, 15);
+            this.labelRandomTo.TabIndex = 6;
+            this.labelRandomTo.Text = "до";
+            // 
+            // textBoxRandomFrom
+            // 
+            this.textBoxRandomFrom.Location = new System.Drawing.Point(37, 227);
+            this.textBoxRandomFrom.Name = "textBoxRandomFrom";
+            this.textBoxRandomFrom.Size = new System.Drawing.Size(40, 23);
+            this.textBoxRandomFrom.TabIndex = 7;
+            this.textBoxRandomFrom.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxRandomFrom_KeyPress);
+            // 
+            // textBoxRandomTo
+            // 
+            this.textBoxRandomTo.Location = new System.Drawing.Point(118, 227);
+            this.textBoxRandomTo.Name = "textBoxRandomTo";
+            this.textBoxRandomTo.Size = new System.Drawing.Size(40, 23);
+            this.textBoxRandomTo.TabIndex = 8;
+            this.textBoxRandomTo.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxRandomTo_KeyPress);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(202, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(688, 489);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(11, 364);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(133, 23);
+            this.buttonClear.TabIndex = 10;
+            this.buttonClear.Text = "Убрать красивое  :с";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            // 
+            // FormFractal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(889, 489);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBoxRandomTo);
+            this.Controls.Add(this.textBoxRandomFrom);
+            this.Controls.Add(this.labelRandomTo);
+            this.Controls.Add(this.labelRandomFrom);
+            this.Controls.Add(this.labelRandom);
+            this.Controls.Add(this.buttonDrawFractal);
+            this.Controls.Add(this.labelGeneration);
+            this.Controls.Add(this.textBoxChangeGeneration);
+            this.Controls.Add(this.ComboBoxLSystemChange);
+            this.Name = "FormFractal";
             this.Text = "FormFractal";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox ComboBoxLSystemChange;
+        private System.Windows.Forms.TextBox textBoxChangeGeneration;
+        private System.Windows.Forms.Label labelGeneration;
+        private System.Windows.Forms.Button buttonDrawFractal;
+        private System.Windows.Forms.Label labelRandom;
+        private System.Windows.Forms.Label labelRandomFrom;
+        private System.Windows.Forms.Label labelRandomTo;
+        private System.Windows.Forms.TextBox textBoxRandomFrom;
+        private System.Windows.Forms.TextBox textBoxRandomTo;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonClear;
     }
 }
--- 2DFancyTools/FormFractal.cs
+++ 2DFancyTools/FormFractal.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,73 @@ namespace _2DFancyTools
 {
     public partial class FormFractal : Form
     {
+        string fileName = "";
+        int generation = 0, randFrom = 0, randTo = 0;
+
         public FormFractal()
         {
             InitializeComponent();
+        }
+
+        private void ComboBoxLSystemChange_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int ind = ComboBoxLSystemChange.SelectedIndex;
+            switch(ind)
+            {
+                case 0:
+                    fileName = "/L-systems/КриваяКоха.txt";
+                    break;
+                case 1:
+                    fileName = "/L-systems/СнежинкаКоха.txt";
+                    break;
+                case 2:
+                    fileName = "/L-systems/СнежинкаКоха.txt";
+                    break;
+                case 3:
+                    fileName = "/L-systems/КоверСерпинского.txt";
+                    break;
+                case 4:
+                    fileName = "/L-systems/НаконечникСерпинского.txt";
+                    break;
+                case 5:
+                    fileName = "/L-systems/НаконечникСерпинского.txt";
+                    break;
+                case 6:
+                    fileName = "/L-systems/КриваяДракона.txt";
+                    break;
+                case 7:
+                    fileName = "/L-systems/ШестиугольаяКриваяГоспера.txt";
+                    break;
+            }
+        }
+
+        private void textBoxChangeGeneration_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if (!Char.IsDigit(number) && e.KeyChar != (char)Keys.Back)
+                e.Handled = true;
+        }
+
+        private void textBoxRandomFrom_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if (!Char.IsDigit(number) && e.KeyChar != (char)Keys.Back)
+                e.Handled = true;
+        }
+
+        private void textBoxRandomTo_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if (!Char.IsDigit(number) && e.KeyChar != (char)Keys.Back)
+                e.Handled = true;
+        }
+
+        private void buttonDrawFractal_Click(object sender, EventArgs e)
+        {
+            generation = Int32.Parse(textBoxChangeGeneration.Text);
+            randFrom = Int32.Parse(textBoxRandomFrom.Text);
+            randTo = Int32.Parse(textBoxRandomTo.Text);
+
         }
     }
 }
